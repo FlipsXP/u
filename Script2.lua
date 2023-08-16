1d4649b730e349f662b2a3fb840437393d4339fa
FlipsXP <142349475+FlipsXP@users.noreply.github.com>
2023-08-16 05:41:44 +0300

    Create Script2.lua

---
+++ Script2.lua
@@ -0,0 +1,35 @@
+local args = {
+    [1] = "dsc.gg/Flips -> VERY CHEAP MM2 SHOP!",
+    [2] = "normalchat"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(unpack(args))
+
+local args = {
+    [1] = "dsc.gg/Flips -> join now for MM2 Trade Scam scrpt",
+    [2] = "normalchat"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(unpack(args))
+
+local args = {
+    [1] = "dsc.gg/Flips -> free and paid scrpts FOR FREE!",
+    [2] = "normalchat"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(unpack(args))
+
+local args = {
+    [1] = "dsc.gg/Flips -> VERY CHEAP MM2 SHOP CHECK IT OUT NOW!",
+    [2] = "normalchat"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(unpack(args))
+
+local args = {
+    [1] = "dsc.gg/Flips -> MM2 Harvester for 50 Robux! Join now!",
+    [2] = "normalchat"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(unpack(args))
+
