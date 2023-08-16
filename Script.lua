f0b951b44a90b7d504bdc4b93e22d0e14601741d
FlipsXP <142349475+FlipsXP@users.noreply.github.com>
2023-08-16 04:12:37 +0300

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,34 @@
+local args = {
+    [1] = "dsc.gg/Flips",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(unpack(args))
+
+local args = {
+    [1] = "dsc.gg/Flips -> free and paid scripts for FREE",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(unpack(args))
+
+local args = {
+    [1] = "dsc.gg/Flips -> join now",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(unpack(args))
+
+local args = {
+    [1] = "dsc.gg/Flips -> epic scripts",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(unpack(args))
+
+local args = {
+    [1] = "dsc.gg/Flips -> join for working mm2 trade scam script",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(unpack(args))
